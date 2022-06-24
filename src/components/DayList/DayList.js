1e2fe05f30fdd94ef38bc44f14175fd00196ca8d
Anastasiya Perova <perovapa@gmail.com>
2022-06-24 16:38:56 +0500

    component icon, getTodosFromResponse

--- src/components/DayList/DayList.js
+++ src/components/DayList/DayList.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
+import { useMemo } from 'react';
 import { useCallback } from 'react';
 import { useState, useEffect } from 'react';
+import DoneIcon from '../../icons/DoneIcon';
 import delIcon from '../../images/delete.svg';
 import delDone from '../../images/deleteDone.svg';
 
 import './DayList.css';
+
+export const TaskStatus = { TODO: 'todo', DONE: 'done' };
 
 export default function DayList() {
   const url = 'http://localhost:3001/todos/';
@@ -45,7 +49,8 @@ export default function DayList() {
         .put(`${url}${item.id}`, {
           id: item.id,
           title: item.title,
-          status: item.status === 'todo' ? 'done' : 'todo',
+          status:
+            item.status === TaskStatus.TODO ? TaskStatus.DONE : TaskStatus.TODO,
         })
         .then(setIsDataChanged(!isDataChanged));
     },
@@ -59,83 +64,53 @@ export default function DayList() {
     [isDataChanged]
   );
 
+  const getTodosFromResponse = useMemo(() => {
+    return todo.map((item) => {
+      return (
+        <li
+          className={`DayList__listItem DayList__listItem_${item.status}`}
+          key={item.id}
+        >
+          <input
+            type='text'
+            placeholder={item.title}
+            onKeyDown={(event) => {
+              if (event.code === 'Enter') {
+                event.target.blur();
+              }
+            }}
+            onBlur={(event) => onBlurChange(event, item)}
+          />
+          <div className='DayList__itemButtons'>
+            <div
+              className='DayList__iconWrapper'
+              onClick={() => onClickDone(item)}
+            >
+              <DoneIcon className='DayList__done' status={item.status} />
+            </div>
+            <img
+              className='DayList__delete'
+              src={delIcon}
+              onClick={() => onClickDelete(item)}
+              alt='Удалить'
+              width='19px'
+              height='20px'
+            />
+          </div>
+        </li>
+      );
+    });
+  }, [todo, onBlurChange, onClickDone, onClickDelete]);
+
   useEffect(() => {
     axios.get(url).then((response) => {
       setTodo(
-        response.data
-          .filter((item) => {
-            return filter === 'all' ? true : filter === item.status;
-          })
-          .map((item) => {
-            return (
-              <li
-                className={`DayList__listItem DayList__listItem_${item.status}`}
-                key={item.id}
-              >
-                <input
-                  type='text'
-                  placeholder={item.title}
-                  onKeyDown={(event) => {
-                    if (event.code === 'Enter') {
-                      event.target.blur();
-                    }
-                  }}
-                  onBlur={(event) => onBlurChange(event, item)}
-                />
-                <div className='DayList__itemButtons'>
-                  <svg
-                    className='DayList__done'
-                    onClick={() => onClickDone(item)}
-                    width='20'
-                    height='20'
-                    viewBox='0 0 200 200'
-                    fill='none'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      clipRule='evenodd'
-                      d='M100 200C155.228 200 200 155.228 200 100C200 44.7715 155.228 0 100 0C44.7715 0 0 44.7715 0 100C0 155.228 44.7715 200 100 200ZM157.36 63.975C159.555 60.4625 158.488 55.8353 154.975 53.64C151.462 51.4447 146.835 52.5125 144.64 56.025L94.5335 136.195L51.194 94.5896C48.2059 91.721 43.4582 91.8179 40.5896 94.806C37.721 97.7941 37.8179 102.542 40.806 105.41L90.806 153.41C92.4355 154.975 94.6831 155.721 96.9247 155.443C99.1662 155.164 101.163 153.89 102.36 151.975L157.36 63.975Z'
-                      fill='url(#paint0_linear_201_4)'
-                    />
-                    <defs>
-                      <linearGradient
-                        id='paint0_linear_201_4'
-                        x1='234'
-                        y1='-16'
-                        x2='24'
-                        y2='182'
-                        gradientUnits='userSpaceOnUse'
-                      >
-                        <stop stopColor='#FF0000' />
-                        <stop offset='1' stopColor='#7729F7' />
-                        <stop offset='1' stopColor='#6100FF' />
-                      </linearGradient>
-                    </defs>
-                  </svg>
-                  <img
-                    className='DayList__delete'
-                    src={delIcon}
-                    onClick={() => onClickDelete(item)}
-                    alt='Удалить'
-                    width='19px'
-                    height='20px'
-                  />
-                </div>
-              </li>
-            );
-          })
+        response.data.filter((item) => {
+          return filter === 'all' ? true : filter === item.status;
+        })
       );
     });
-  }, [
-    setTodo,
-    setIsDataChanged,
-    isDataChanged,
-    filter,
-    onBlurChange,
-    onClickDone,
-    onClickDelete,
-  ]);
+  }, [setTodo, filter, isDataChanged]);
 
   return (
     <div className='DayList'>
@@ -144,7 +119,7 @@ export default function DayList() {
         <p className='DayList__weekday'>{days[date.getDay()]}</p>
       </div>
       <ul className='DayList__list'>
-        {todo}
+        {getTodosFromResponse}
         <li className='DayList__listItem DayList__listItem_blank'>
           <input
             type='text'
